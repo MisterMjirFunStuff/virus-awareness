c086c7b9baac0aa674c7d2d7c2f58be9a12993c4
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2020-05-09 20:42:15 -0500

    Add files via upload

---
+++ important.txt.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+ECHO Bruh, this could've been a virus. Be more careful fool!
+ECHO When looking at a file type, only the last dot counts as the extension.
+ECHO Watch out for executable file extensions.
+ECHO For example they can delete your files (foreshadow)
+
+PAUSE
+
+DEL important.txt.bat
